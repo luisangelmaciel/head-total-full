3c13a231d92d18a45f3b2f6bc1c12cfd11b58d64
luisangelmaciel <89158021+luisangelmaciel@users.noreply.github.com>
2021-08-22 11:23:55 -0500

    Add files via upload

---
+++ wam-pack/sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'xivit';
+var filesToCache = [
+    '/',
+    '/room.ejs',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js',
+    '/manifest.json'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
